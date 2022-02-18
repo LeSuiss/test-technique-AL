a5f76af8d47592f4ba79a37dc08b3f4636dc8c3a
alexis archer <alexis.archer@5asec.fr>
2022-02-18 09:32:45 +0100

    adding jest test for API

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -1,3 +1,10 @@
-test('démo',()=>
-expect(1+1).toEqual(2))
+const request = require("supertest");
+const app = require("./app");
 
+
+test('API call is returning a JSON',()=>request(app)
+  .get("/getMovieList")
+  .send({title:"the godfather"})
+  .expect("Content-Type", /json/)
+  .expect(200)
+)
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import express from 'express';
 import { Request, Response } from 'express';
 const app = express();
-const port = 3000;
+const port = 3002;
 const Redis = require("ioredis");
 const redis = new Redis(); // uses defaults unless given configuration object
 const env = require('dotenv').config().parsed
@@ -12,15 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
-
 app.get('/getMovieList', async(req: Request, res:Response) => {
   try {  
     const query = req.body
-    let urlSearchQuery = `${env.API_URL}/?apikey=${env.API_KEY}&s=true`
+    let urlSearchQuery = `${env.API_URL}/?apikey=${env.API_KEY}`
     
     Object.keys(query).forEach(function enhanceUrlWithQuery(key:string):string|void{
       if(!!query[key]){
-        return urlSearchQuery += `&?${key}=${query[key]}`
+        let formatedValue = query[key].replaceAll(' ', '+')
+        return urlSearchQuery += `&${key}=${formatedValue}`
       }
     })
 
@@ -48,4 +48,7 @@ app.get('/getMovieList', async(req: Request, res:Response) => {
 });
 
 
-app.listen(port, ()=>console.log('listening on port 3000'))
+app.listen(port, ()=>console.log('listening on port '+port))
+
+
+module.exports = app;
